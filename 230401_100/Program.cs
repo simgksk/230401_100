1a5ddbb1af8ace32f121807aa939752eb3bb93d9
simgksk <Daum_c0m@naver.com>
2023-04-01 23:35:35 +0900

    초기 커밋.

---
+++ 230401_100/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _230401_100
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int year = int.Parse(Console.ReadLine());
+
+            if (year % 4 == 0 || year != 100 && year % 400 == 0)
+            {
+                Console.WriteLine("1");
+            }
+            else
+            {
+                Console.WriteLine("0");
+            }
+        }
+    }
+}
